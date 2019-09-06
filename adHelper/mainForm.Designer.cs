1c3e1d471603e2bac67441ad3c8032b60f5808ab
dwljia <dwljia@qq.com>
2019-09-07 01:43:45 +0800

    init
    init

---
+++ adHelper/mainForm.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace adHelper
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGetImgCss = new System.Windows.Forms.TextBox();
+            this.tabCtrlMain = new System.Windows.Forms.TabControl();
+            this.tabpgPtoCss = new System.Windows.Forms.TabPage();
+            this.tabpgHtml = new System.Windows.Forms.TabPage();
+            this.txtGetImgHtml = new System.Windows.Forms.TextBox();
+            this.tabpgSetting = new System.Windows.Forms.TabPage();
+            this.gbSet1 = new System.Windows.Forms.GroupBox();
+            this.cbxIsMerge = new System.Windows.Forms.CheckBox();
+            this.lab1 = new System.Windows.Forms.Label();
+            this.txtGlobalClass = new System.Windows.Forms.TextBox();
+            this.lab2 = new System.Windows.Forms.Label();
+            this.lab3 = new System.Windows.Forms.Label();
+            this.cboxUnit = new System.Windows.Forms.ComboBox();
+            this.txtRemMultiple = new System.Windows.Forms.TextBox();
+            this.tabCtrlMain.SuspendLayout();
+            this.tabpgPtoCss.SuspendLayout();
+            this.tabpgHtml.SuspendLayout();
+            this.tabpgSetting.SuspendLayout();
+            this.gbSet1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtGetImgCss
+            // 
+            this.txtGetImgCss.AllowDrop = true;
+            this.txtGetImgCss.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtGetImgCss.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtGetImgCss.Location = new System.Drawing.Point(2, 4);
+            this.txtGetImgCss.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.txtGetImgCss.Multiline = true;
+            this.txtGetImgCss.Name = "txtGetImgCss";
+            this.txtGetImgCss.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtGetImgCss.Size = new System.Drawing.Size(612, 404);
+            this.txtGetImgCss.TabIndex = 0;
+            this.txtGetImgCss.WordWrap = false;
+            this.txtGetImgCss.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtGetImgCss_DragDrop);
+            this.txtGetImgCss.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtGetImgCss_DragEnter);
+            this.txtGetImgCss.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtGetImgCss_KeyDown);
+            // 
+            // tabCtrlMain
+            // 
+            this.tabCtrlMain.Controls.Add(this.tabpgPtoCss);
+            this.tabCtrlMain.Controls.Add(this.tabpgHtml);
+            this.tabCtrlMain.Controls.Add(this.tabpgSetting);
+            this.tabCtrlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabCtrlMain.Location = new System.Drawing.Point(0, 0);
+            this.tabCtrlMain.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.tabCtrlMain.Name = "tabCtrlMain";
+            this.tabCtrlMain.SelectedIndex = 0;
+            this.tabCtrlMain.Size = new System.Drawing.Size(624, 442);
+            this.tabCtrlMain.TabIndex = 1;
+            // 
+            // tabpgPtoCss
+            // 
+            this.tabpgPtoCss.Controls.Add(this.txtGetImgCss);
+            this.tabpgPtoCss.Location = new System.Drawing.Point(4, 26);
+            this.tabpgPtoCss.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.tabpgPtoCss.Name = "tabpgPtoCss";
+            this.tabpgPtoCss.Padding = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.tabpgPtoCss.Size = new System.Drawing.Size(616, 412);
+            this.tabpgPtoCss.TabIndex = 0;
+            this.tabpgPtoCss.Text = "CSS";
+            this.tabpgPtoCss.UseVisualStyleBackColor = true;
+            // 
+            // tabpgHtml
+            // 
+            this.tabpgHtml.Controls.Add(this.txtGetImgHtml);
+            this.tabpgHtml.Location = new System.Drawing.Point(4, 26);
+            this.tabpgHtml.Name = "tabpgHtml";
+            this.tabpgHtml.Size = new System.Drawing.Size(616, 412);
+            this.tabpgHtml.TabIndex = 2;
+            this.tabpgHtml.Text = "HTML";
+            this.tabpgHtml.UseVisualStyleBackColor = true;
+            // 
+            // txtGetImgHtml
+            // 
+            this.txtGetImgHtml.AllowDrop = true;
+            this.txtGetImgHtml.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtGetImgHtml.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtGetImgHtml.Location = new System.Drawing.Point(0, 0);
+            this.txtGetImgHtml.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.txtGetImgHtml.Multiline = true;
+            this.txtGetImgHtml.Name = "txtGetImgHtml";
+            this.txtGetImgHtml.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtGetImgHtml.Size = new System.Drawing.Size(616, 412);
+            this.txtGetImgHtml.TabIndex = 1;
+            this.txtGetImgHtml.WordWrap = false;
+            // 
+            // tabpgSetting
+            // 
+            this.tabpgSetting.Controls.Add(this.gbSet1);
+            this.tabpgSetting.Location = new System.Drawing.Point(4, 26);
+            this.tabpgSetting.Name = "tabpgSetting";
+            this.tabpgSetting.Size = new System.Drawing.Size(616, 412);
+            this.tabpgSetting.TabIndex = 1;
+            this.tabpgSetting.Text = "设置";
+            this.tabpgSetting.UseVisualStyleBackColor = true;
+            // 
+            // gbSet1
+            // 
+            this.gbSet1.Controls.Add(this.cbxIsMerge);
+            this.gbSet1.Controls.Add(this.lab1);
+            this.gbSet1.Controls.Add(this.txtGlobalClass);
+            this.gbSet1.Controls.Add(this.lab2);
+            this.gbSet1.Controls.Add(this.lab3);
+            this.gbSet1.Controls.Add(this.cboxUnit);
+            this.gbSet1.Controls.Add(this.txtRemMultiple);
+            this.gbSet1.Location = new System.Drawing.Point(16, 16);
+            this.gbSet1.Name = "gbSet1";
+            this.gbSet1.Size = new System.Drawing.Size(216, 192);
+            this.gbSet1.TabIndex = 6;
+            this.gbSet1.TabStop = false;
+            this.gbSet1.Text = "公 共";
+            // 
+            // cbxIsMerge
+            // 
+            this.cbxIsMerge.AutoSize = true;
+            this.cbxIsMerge.Location = new System.Drawing.Point(69, 113);
+            this.cbxIsMerge.Name = "cbxIsMerge";
+            this.cbxIsMerge.Size = new System.Drawing.Size(87, 21);
+            this.cbxIsMerge.TabIndex = 6;
+            this.cbxIsMerge.Text = "多图则组合";
+            this.cbxIsMerge.UseVisualStyleBackColor = true;
+            // 
+            // lab1
+            // 
+            this.lab1.AutoSize = true;
+            this.lab1.Location = new System.Drawing.Point(28, 26);
+            this.lab1.Name = "lab1";
+            this.lab1.Size = new System.Drawing.Size(35, 17);
+            this.lab1.TabIndex = 0;
+            this.lab1.Text = "单位:";
+            // 
+            // txtGlobalClass
+            // 
+            this.txtGlobalClass.Location = new System.Drawing.Point(69, 82);
+            this.txtGlobalClass.Name = "txtGlobalClass";
+            this.txtGlobalClass.Size = new System.Drawing.Size(121, 23);
+            this.txtGlobalClass.TabIndex = 5;
+            this.txtGlobalClass.Text = "cimg";
+            this.txtGlobalClass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lab2
+            // 
+            this.lab2.AutoSize = true;
+            this.lab2.Location = new System.Drawing.Point(5, 56);
+            this.lab2.Name = "lab2";
+            this.lab2.Size = new System.Drawing.Size(58, 17);
+            this.lab2.TabIndex = 2;
+            this.lab2.Text = "rem倍数:";
+            // 
+            // lab3
+            // 
+            this.lab3.AutoSize = true;
+            this.lab3.Location = new System.Drawing.Point(16, 85);
+            this.lab3.Name = "lab3";
+            this.lab3.Size = new System.Drawing.Size(47, 17);
+            this.lab3.TabIndex = 4;
+            this.lab3.Text = "公共类:";
+            // 
+            // cboxUnit
+            // 
+            this.cboxUnit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxUnit.FormattingEnabled = true;
+            this.cboxUnit.Items.AddRange(new object[] {
+            "rem",
+            "px",
+            "em",
+            "%"});
+            this.cboxUnit.Location = new System.Drawing.Point(69, 22);
+            this.cboxUnit.Name = "cboxUnit";
+            this.cboxUnit.Size = new System.Drawing.Size(121, 25);
+            this.cboxUnit.TabIndex = 1;
+            this.cboxUnit.SelectedIndexChanged += new System.EventHandler(this.cboxUnit_SelectedIndexChanged);
+            // 
+            // txtRemMultiple
+            // 
+            this.txtRemMultiple.Location = new System.Drawing.Point(69, 53);
+            this.txtRemMultiple.Name = "txtRemMultiple";
+            this.txtRemMultiple.Size = new System.Drawing.Size(121, 23);
+            this.txtRemMultiple.TabIndex = 3;
+            this.txtRemMultiple.Text = "100";
+            this.txtRemMultiple.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // mainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.tabCtrlMain);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.Name = "mainForm";
+            this.Text = "adHelper";
+            this.tabCtrlMain.ResumeLayout(false);
+            this.tabpgPtoCss.ResumeLayout(false);
+            this.tabpgPtoCss.PerformLayout();
+            this.tabpgHtml.ResumeLayout(false);
+            this.tabpgHtml.PerformLayout();
+            this.tabpgSetting.ResumeLayout(false);
+            this.gbSet1.ResumeLayout(false);
+            this.gbSet1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGetImgCss;
+        private System.Windows.Forms.TabControl tabCtrlMain;
+        private System.Windows.Forms.TabPage tabpgPtoCss;
+        private System.Windows.Forms.TabPage tabpgSetting;
+        private System.Windows.Forms.Label lab1;
+        private System.Windows.Forms.ComboBox cboxUnit;
+        private System.Windows.Forms.TextBox txtRemMultiple;
+        private System.Windows.Forms.Label lab2;
+        private System.Windows.Forms.TextBox txtGlobalClass;
+        private System.Windows.Forms.Label lab3;
+        private System.Windows.Forms.GroupBox gbSet1;
+        private System.Windows.Forms.CheckBox cbxIsMerge;
+        private System.Windows.Forms.TabPage tabpgHtml;
+        private System.Windows.Forms.TextBox txtGetImgHtml;
+    }
+}
+
---
+++ adHelper/mainForm.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adHelper {
+    struct CssSize {
+        public string Width;
+        public string Height;
+    }
+    struct GCtrl {
+        public string MergeClassName;
+    }
+    public partial class mainForm : Form {
+        //全局设置
+        GCtrl gCtrl = new GCtrl();
+        public mainForm() {
+            InitializeComponent();
+            //初始化设置
+            cboxUnit.SelectedIndex = 0;
+            //全局设置
+            gCtrl.MergeClassName = "_mergeExample_";
+        }
+
+        private void txtGetImgCss_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Modifiers == Keys.Control && e.KeyCode == Keys.A) {
+                ((TextBox)sender).SelectAll();
+            }
+        }
+
+        private void txtGetImgCss_DragDrop(object sender, DragEventArgs e) {
+            Array dragData = (System.Array)e.Data.GetData(DataFormats.FileDrop);
+            System.Text.StringBuilder strCss = new System.Text.StringBuilder(),
+                strHtml = new System.Text.StringBuilder();
+            for (var i = 0; i < dragData.Length; i++) {
+                strCss.AppendLine(getCss(dragData.GetValue(i).ToString()));
+                strHtml.AppendLine(getHtml(dragData.GetValue(i).ToString()));
+            }
+            if (cbxIsMerge.Checked && dragData.Length > 1) {
+                strCss.AppendLine(getMergeCss(dragData));
+                strHtml.Insert(0, "<div class=\"" + gCtrl.MergeClassName + "\">\r\n");
+                strHtml.Insert(0, "<!-- " + gCtrl.MergeClassName + " -->\r\n");
+                strHtml.AppendLine("</div>");
+            }
+            txtGetImgCss.Text = strCss.ToString();
+            txtGetImgHtml.Text = strHtml.ToString();
+            txtGetImgCss.SelectAll();
+        }
+
+        private void txtGetImgCss_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+                e.Effect = DragDropEffects.Link;
+            } else {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void cboxUnit_SelectedIndexChanged(object sender, EventArgs e) {
+            switch (cboxUnit.Text) {
+                case "rem":
+                    txtRemMultiple.Enabled = true;
+                    lab2.Enabled = true;
+                    break;
+                default:
+                    txtRemMultiple.Enabled = false;
+                    lab2.Enabled = false;
+                    break;
+            }
+        }
+
+        //生成cimg css
+        private string getCss(string url) {
+            Image imgPto = Image.FromFile(url);
+            CssSize csPto = getPtoSizeInCss(imgPto.Size);
+            System.Text.StringBuilder strCss = new System.Text.StringBuilder();
+            string strTab = "    ";
+            imgPto.Dispose();
+            strCss.AppendLine("/* " + txtGlobalClass.Text + ">" + System.IO.Path.GetFileNameWithoutExtension(url) + " */");
+            strCss.AppendLine("." + txtGlobalClass.Text + "." + System.IO.Path.GetFileNameWithoutExtension(url) + " {");
+            strCss.AppendLine(strTab + "background-image: url(\"../images/" + System.IO.Path.GetFileName(url) + "\");");
+            strCss.AppendLine(strTab + "width: " + csPto.Width + ";");
+            strCss.AppendLine(strTab + "height: " + csPto.Height + ";");
+            strCss.AppendLine("}");
+            return strCss.ToString();
+        }
+
+        //生成组合css
+        private string getMergeCss(Array urls) {
+            System.Text.StringBuilder strCss = new System.Text.StringBuilder();
+            int intMaxWidth = 0,
+                intMaxHeight = 0;
+            string strTab = "    ";
+            string[] strNames = new string[urls.Length];
+            Image _tmpImg;
+            CssSize _tmpCSize = new CssSize();
+            for (int i = 0; i < urls.Length; i++) {
+                _tmpImg = Image.FromFile(urls.GetValue(i).ToString());
+                if (_tmpImg.Width > intMaxWidth) {
+                    intMaxWidth = _tmpImg.Width;
+                }
+                if (_tmpImg.Height > intMaxHeight) {
+                    intMaxHeight = _tmpImg.Height;
+                }
+                _tmpImg.Dispose();
+                strNames[i] = System.IO.Path.GetFileNameWithoutExtension(urls.GetValue(i).ToString());
+            }
+            _tmpCSize = getPtoSizeInCss(new Size(intMaxWidth, intMaxHeight));
+            strCss.AppendLine("/* merge>" + gCtrl.MergeClassName + " */");
+            strCss.AppendLine("." + gCtrl.MergeClassName + " {");
+            strCss.AppendLine(strTab + "width: " + _tmpCSize.Width + ";");
+            strCss.AppendLine(strTab + "height: " + _tmpCSize.Height + ";");
+            strCss.AppendLine(strTab + "position: relative;");
+            strCss.AppendLine("}");
+            for (int i = 0; i < strNames.Length; i++) {
+                strCss.Append("." + gCtrl.MergeClassName + " ." + strNames[i]);
+                if (i < strNames.Length - 1) {
+                    strCss.AppendLine(",");
+                } else {
+                    strCss.AppendLine("{");
+                }
+            }
+            strCss.AppendLine(strTab + "position: absolute;");
+            strCss.AppendLine("}");
+            strCss.AppendLine();
+            for (int i = 0; i < strNames.Length; i++) {
+                strCss.AppendLine("." + gCtrl.MergeClassName + " ." + strNames[i] + "{");
+                strCss.AppendLine(strTab + "left: 0;");
+                strCss.AppendLine(strTab + "top: 0;");
+                strCss.AppendLine("}");
+                strCss.AppendLine();
+            }
+            return strCss.ToString();
+        }
+
+        //获取文件名字集合
+        private string[] getNamesByUrls(string[] urls) {
+            string[] strNames = new string[urls.Length];
+            for (int i = 0; i < urls.Length; i++) {
+                strNames[i] = System.IO.Path.GetFileNameWithoutExtension(urls[i]);
+            }
+            return strNames;
+        }
+
+        //获取尺寸
+        private CssSize getPtoSizeInCss(Size sizeImg) {
+            CssSize sizePto = new CssSize();
+            switch (cboxUnit.Text) {
+                case "rem":
+                    sizePto.Width = ((double)sizeImg.Width / Convert.ToDouble(txtRemMultiple.Text)).ToString();
+                    sizePto.Height = ((double)sizeImg.Height / Convert.ToDouble(txtRemMultiple.Text)).ToString();
+                    break;
+                case "px":
+                    sizePto.Width = sizeImg.Width.ToString();
+                    sizePto.Height = sizeImg.Height.ToString();
+                    break;
+                case "em":
+                    sizePto.Width = sizeImg.Width.ToString();
+                    sizePto.Height = sizeImg.Height.ToString();
+                    break;
+                case "%":
+                    sizePto.Width = sizeImg.Width.ToString();
+                    sizePto.Height = sizeImg.Height.ToString();
+                    break;
+                default:
+                    sizePto.Width = sizeImg.Width.ToString();
+                    sizePto.Height = sizeImg.Height.ToString();
+                    break;
+            }
+            sizePto.Width += cboxUnit.Text;
+            sizePto.Height += cboxUnit.Text;
+            return sizePto;
+        }
+
+        //生成html
+        private string getHtml(string url) {
+            System.Text.StringBuilder strHtml = new System.Text.StringBuilder();
+            strHtml.AppendLine("<!-- " + System.IO.Path.GetFileNameWithoutExtension(url) + " -->");
+            strHtml.Append("<span class=\"cimg " + System.IO.Path.GetFileNameWithoutExtension(url) + "\"></span>");
+            return strHtml.ToString();
+        }
+    }
+}
